40ce483dddd96a7c49fb6a3d54115d9ea510a75c
KLYCHub <klychub@gmail.com>
2022-11-19 13:17:31 +0300

    Visual Programming Assignment
    2 or 4 player horse racing game I made for a visual programming assignment

---
+++ 212701035_eren_kalayci_at_yarisi/at_yarisi/at_yarisi/Form2.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace at_yarisi
+{
+    partial class Form2_21701035
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2_21701035));
+            this.textBox1_21701035 = new System.Windows.Forms.TextBox();
+            this.button1_21701035 = new System.Windows.Forms.Button();
+            this.panel1_21701035 = new System.Windows.Forms.Panel();
+            this.label2_21701035 = new System.Windows.Forms.Label();
+            this.label1_21701035 = new System.Windows.Forms.Label();
+            this.button3_21701035 = new System.Windows.Forms.Button();
+            this.button2_21701035 = new System.Windows.Forms.Button();
+            this.panel1_21701035.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1_21701035
+            // 
+            this.textBox1_21701035.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1_21701035.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1_21701035.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1_21701035.Location = new System.Drawing.Point(101, 20);
+            this.textBox1_21701035.Name = "textBox1_21701035";
+            this.textBox1_21701035.Size = new System.Drawing.Size(76, 31);
+            this.textBox1_21701035.TabIndex = 0;
+            this.textBox1_21701035.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1_21701035
+            // 
+            this.button1_21701035.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1_21701035.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1_21701035.ForeColor = System.Drawing.Color.White;
+            this.button1_21701035.Location = new System.Drawing.Point(183, 17);
+            this.button1_21701035.Name = "button1_21701035";
+            this.button1_21701035.Size = new System.Drawing.Size(88, 37);
+            this.button1_21701035.TabIndex = 1;
+            this.button1_21701035.Text = "BAŞLA";
+            this.button1_21701035.UseVisualStyleBackColor = false;
+            this.button1_21701035.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1_21701035
+            // 
+            this.panel1_21701035.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1_21701035.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1_21701035.Controls.Add(this.label2_21701035);
+            this.panel1_21701035.Controls.Add(this.label1_21701035);
+            this.panel1_21701035.Controls.Add(this.button1_21701035);
+            this.panel1_21701035.Controls.Add(this.textBox1_21701035);
+            this.panel1_21701035.Location = new System.Drawing.Point(-13, -4);
+            this.panel1_21701035.Name = "panel1_21701035";
+            this.panel1_21701035.Size = new System.Drawing.Size(1005, 63);
+            this.panel1_21701035.TabIndex = 2;
+            this.panel1_21701035.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2_21701035
+            // 
+            this.label2_21701035.AutoSize = true;
+            this.label2_21701035.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2_21701035.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2_21701035.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2_21701035.Location = new System.Drawing.Point(719, 22);
+            this.label2_21701035.Margin = new System.Windows.Forms.Padding(5);
+            this.label2_21701035.Name = "label2_21701035";
+            this.label2_21701035.Padding = new System.Windows.Forms.Padding(3);
+            this.label2_21701035.Size = new System.Drawing.Size(244, 29);
+            this.label2_21701035.TabIndex = 3;
+            this.label2_21701035.Text = "Eren KALAYCI - 212701035";
+            // 
+            // label1_21701035
+            // 
+            this.label1_21701035.AutoSize = true;
+            this.label1_21701035.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1_21701035.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1_21701035.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1_21701035.Location = new System.Drawing.Point(27, 20);
+            this.label1_21701035.Margin = new System.Windows.Forms.Padding(5);
+            this.label1_21701035.Name = "label1_21701035";
+            this.label1_21701035.Padding = new System.Windows.Forms.Padding(3);
+            this.label1_21701035.Size = new System.Drawing.Size(66, 29);
+            this.label1_21701035.TabIndex = 2;
+            this.label1_21701035.Text = "GİRİŞ";
+            // 
+            // button3_21701035
+            // 
+            this.button3_21701035.BackColor = System.Drawing.Color.Black;
+            this.button3_21701035.ForeColor = System.Drawing.Color.White;
+            this.button3_21701035.Location = new System.Drawing.Point(905, 522);
+            this.button3_21701035.Name = "button3_21701035";
+            this.button3_21701035.Size = new System.Drawing.Size(56, 23);
+            this.button3_21701035.TabIndex = 5;
+            this.button3_21701035.Text = "STOP";
+            this.button3_21701035.UseVisualStyleBackColor = false;
+            this.button3_21701035.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2_21701035
+            // 
+            this.button2_21701035.BackColor = System.Drawing.Color.Black;
+            this.button2_21701035.ForeColor = System.Drawing.Color.White;
+            this.button2_21701035.Location = new System.Drawing.Point(828, 522);
+            this.button2_21701035.Name = "button2_21701035";
+            this.button2_21701035.Size = new System.Drawing.Size(71, 23);
+            this.button2_21701035.TabIndex = 4;
+            this.button2_21701035.Text = "RESET";
+            this.button2_21701035.UseVisualStyleBackColor = false;
+            this.button2_21701035.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form2_21701035
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::at_yarisi.Properties.Resources.dark_horse_animals_black_wallpaper1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(964, 557);
+            this.Controls.Add(this.button3_21701035);
+            this.Controls.Add(this.button2_21701035);
+            this.Controls.Add(this.panel1_21701035);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2_21701035";
+            this.Text = "GİRİŞ EKRANI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel1_21701035.ResumeLayout(false);
+            this.panel1_21701035.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1_21701035;
+        private System.Windows.Forms.Button button1_21701035;
+        private System.Windows.Forms.Panel panel1_21701035;
+        private System.Windows.Forms.Label label1_21701035;
+        private System.Windows.Forms.Label label2_21701035;
+        private System.Windows.Forms.Button button3_21701035;
+        private System.Windows.Forms.Button button2_21701035;
+    }
+}
